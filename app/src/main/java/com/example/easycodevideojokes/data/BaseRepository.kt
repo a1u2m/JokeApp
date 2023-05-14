27356dcabd598e88af5e9a4209aa4b84d4f43211
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-05-14 23:03:38 +0500

    bugfix

--- app/src/main/java/com/example/easycodevideojokes/data/BaseRepository.kt
+++ app/src/main/java/com/example/easycodevideojokes/data/BaseRepository.kt
@@ -6,7 +6,6 @@ import com.example.easycodevideojokes.data.cloud.CloudDataSource
 import com.example.easycodevideojokes.data.cloud.JokeCloud
 import com.example.easycodevideojokes.data.cloud.JokeCloudCallback
 import com.example.easycodevideojokes.presentation.JokeUi
-import com.example.easycodevideojokes.presentation.ManageResources
 
 class BaseRepository(
     private val cloudDataSource: CloudDataSource,
@@ -14,13 +13,14 @@ class BaseRepository(
 ) : Repository<JokeUi, Error> {
 
     private var callback: ResultCallback<JokeUi, Error>? = null
-    private var jokeCloudCached: JokeCloud? = null
+    private var jokeCloudTemporary: JokeCloud? = null
 
     override fun fetch() {
         if (getJokeFromCache) {
 
             cacheDataSource.fetch(object : JokeCacheCallback {
                 override fun provideJoke(joke: JokeCloud) {
+                    jokeCloudTemporary = joke
                     callback?.provideSuccess(joke.toFavoriteUi())
                 }
 
@@ -31,12 +31,12 @@ class BaseRepository(
         } else
             cloudDataSource.fetch(object : JokeCloudCallback {
                 override fun provideJokeCloud(jokeCloud: JokeCloud) {
-                    jokeCloudCached = jokeCloud
+                    jokeCloudTemporary = jokeCloud
                     callback?.provideSuccess(jokeCloud.toUi())
                 }
 
                 override fun provideError(error: Error) {
-                    jokeCloudCached = null
+                    jokeCloudTemporary = null
                     callback?.provideError(error)
                 }
             })
@@ -47,7 +47,7 @@ class BaseRepository(
     }
 
     override fun changeJokeStatus(resultCallback: ResultCallback<JokeUi, Error>) {
-        jokeCloudCached?.let {
+        jokeCloudTemporary?.let {
             resultCallback.provideSuccess(it.change(cacheDataSource))
         }
 
